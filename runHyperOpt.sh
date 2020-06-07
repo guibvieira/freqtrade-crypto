82f301ea2f01184cb075e331cd3a32727c3b2d92
guibvieira <guibvieira@gmail.com>
2020-06-07 11:01:22 +0100

    add more strategy implementations

---
+++ runHyperOpt.sh
@@ -0,0 +1,10 @@
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/1_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/2_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/3_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/4_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/5_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/6_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/7_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/8_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/9_Optimisation.txt
+freqtrade hyperopt -c config.json -s BbandRsi --hyperopt BbandRsiOpt --hyperopt-loss SharpeHyperOptLossDaily -e 1000 --space all --timerange=-20200606 >> ./optimisation-results/10_Optimisation.txt
